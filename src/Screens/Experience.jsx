548f9070722bbd5b19446a7c27efd56a9480a7f7
Your Name <you@example.com>
2023-05-25 15:12:52 +0100

    added new features to experience page

--- src/Screens/Experience.jsx
+++ src/Screens/Experience.jsx
@@ -1,27 +1,55 @@
 import React from "react";
 import { experiences } from "../Constants";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
 
 const Experience = () => {
-	return (
-		<section className={" w-full bg-aboutBg parralaxDemoDiv"}>
-			<div>
-				<h3 className='activeText text-center text-4xl font-bold'>
-					Experience{" "}
-				</h3>
-				<div className='text-center'>
-					{experiences.map((exp, ind) => (
-						<div key={ind}>
-							<p>{exp.company_name}</p>
-							<p>{exp.date}</p>
-							<p>{exp.icon}</p>
-							<p>{exp.points}</p>
-							<p>{exp.title}</p>
-						</div>
-					))}
-				</div>
-			</div>
-		</section>
-	);
+  const responsive = {
+    superLargeDesktop: {
+      breakpoint: { max: 4000, min: 1024 },
+      items: 1,
+      slidesToSlide: 1,
+    },
+    desktop: {
+      breakpoint: { max: 1024, min: 800 },
+      items: 4,
+    },
+    tablet: {
+      breakpoint: { max: 800, min: 464 },
+      items: 2,
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1,
+    },
+  };
+
+  const experience = experiences.map((exp, ind) => (
+    <div key={ind} className="mb-6">
+      <p>{exp.company_name}</p>
+      <p>{exp.date}</p>
+      <p>{exp.icon}</p>
+      <p>{exp.description}</p>
+      <p>{exp.title}</p>
+    </div>
+  ));
+  return (
+    <section className={" w-full bg-aboutBg parralaxDemoDiv"}>
+      <div>
+        <h3 className="activeText text-center text-4xl font-bold">
+          Experience{" "}
+        </h3>
+        <Carousel
+          responsive={responsive}
+          showDots={true}
+          className="text-center my-12"
+        >
+          {experience}
+        </Carousel>
+        ;
+      </div>
+    </section>
+  );
 };
 
 export default Experience;
--- src/constants/index.js
+++ src/constants/index.js
@@ -1,151 +1,155 @@
 import {
-	mobile,
-	web,
-	javascript,
-	typescript,
-	html,
-	css,
-	bootstrap,
-	firebase,
-	reactjs,
-	tailwind,
-	nodejs,
-	git,
-	figma,
-	shopify,
-	carrent,
-	jobit,
-	tripguide,
+  mobile,
+  web,
+  javascript,
+  typescript,
+  html,
+  css,
+  bootstrap,
+  firebase,
+  reactjs,
+  tailwind,
+  nodejs,
+  git,
+  figma,
+  shopify,
+  carrent,
+  jobit,
+  tripguide,
 } from "../assets";
 
 export const navLinks = [
-	{
-		id: "about",
-		title: "About",
-	},
-	{
-		id: "tech",
-		title: "Tech",
-	},
-	{
-		id: "experience",
-		title: "Experience",
-	},
-	{
-		id: "contact",
-		title: "Contact",
-	},
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "tech",
+    title: "Tech",
+  },
+  {
+    id: "experience",
+    title: "Experience",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
 ];
 
 const services = [
-	{
-		title: "Web Developer",
-		icon: web,
-	},
-	{
-		title: "React Js Developer",
-		icon: mobile,
-	},
+  {
+    title: "Web Developer",
+    icon: web,
+  },
+  {
+    title: "React Js Developer",
+    icon: mobile,
+  },
 ];
 
 const technologies = [
-	{
-		name: "HTML 5",
-		icon: html,
-	},
-	{
-		name: "CSS 3",
-		icon: css,
-	},
-	{
-		name: "JavaScript",
-		icon: javascript,
-	},
-	{
-		name: "TypeScript",
-		icon: typescript,
-	},
-	{
-		name: "React JS",
-		icon: reactjs,
-	},
+  {
+    name: "HTML 5",
+    icon: html,
+  },
+  {
+    name: "CSS 3",
+    icon: css,
+  },
+  {
+    name: "JavaScript",
+    icon: javascript,
+  },
+  {
+    name: "TypeScript",
+    icon: typescript,
+  },
+  {
+    name: "React JS",
+    icon: reactjs,
+  },
 
-	{
-		name: "Tailwind CSS",
-		icon: tailwind,
-	},
-	{
-		name: "Bootstrap",
-		icon: bootstrap,
-	},
-	// {
-	// 	name: "Node JS",
-	// 	icon: nodejs,
-	// },
-	{
-		name: "FireBase",
-		icon: firebase,
-	},
-	// {
-	// 	name: "git",
-	// 	icon: git,
-	// },
-	// {
-	// 	name: "figma",
-	// 	icon: figma,
-	// },
+  {
+    name: "Tailwind CSS",
+    icon: tailwind,
+  },
+  {
+    name: "Bootstrap",
+    icon: bootstrap,
+  },
+  // {
+  // 	name: "Node JS",
+  // 	icon: nodejs,
+  // },
+  {
+    name: "FireBase",
+    icon: firebase,
+  },
+  // {
+  // 	name: "git",
+  // 	icon: git,
+  // },
+  // {
+  // 	name: "figma",
+  // 	icon: figma,
+  // },
 ];
 const aboutTech = [
-	{
-		name: "HTML 5",
-		icon: html,
-	},
-	{
-		name: "CSS 3",
-		icon: css,
-	},
-	{
-		name: "JavaScript",
-		icon: javascript,
-	},
+  {
+    name: "HTML 5",
+    icon: html,
+  },
+  {
+    name: "CSS 3",
+    icon: css,
+  },
+  {
+    name: "JavaScript",
+    icon: javascript,
+  },
 
-	{
-		name: "Tailwind CSS",
-		icon: tailwind,
-	},
-	{
-		name: "Bootstrap",
-		icon: bootstrap,
-	},
+  {
+    name: "Tailwind CSS",
+    icon: tailwind,
+  },
+  {
+    name: "Bootstrap",
+    icon: bootstrap,
+  },
 ];
 
 const experiences = [
-	{
-		title: "React.js Developer",
-		company_name: "Younglings",
-		// icon: Younglings,
-		iconBg: "#383E56",
-		date: "March 2020 - April 2021",
-		points: [
-			"Developing and maintaining web applications using React.js and other related technologies.",
-			"Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-			"Implementing responsive design and ensuring cross-browser compatibility.",
-			"Participating in code reviews and providing constructive feedback to other developers.",
-		],
-	},
-	{
-		title: "Web Developer",
-		company_name: "Younglings",
-		// icon: shopify,
-		iconBg: "#383E56",
-		date: "Jan 2022 - Jan 2023",
-		points: [
-			"Developing and maintaining web applications using React.js and other related technologies.",
-			"Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-			"Implementing responsive design and ensuring cross-browser compatibility.",
-			"Participating in code reviews and providing constructive feedback to other developers.",
-		],
-	},
+  {
+    title: "React.js Developer",
+    company_name: "Younglings",
+    // icon: Younglings,
+    iconBg: "#383E56",
+    date: "March 2020 - April 2021",
+    description: [
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged",
+    ],
+  },
+  {
+    title: "Web Developer",
+    company_name: "Ixperience",
+    // icon: Younglings,
+    iconBg: "#383E56",
+    date: "Jan 2022 - Jan 2023",
+    description: [
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged",
+    ],
+  },
+  {
+    title: "Web Developer",
+    company_name: "YDS",
+    // icon: Younglings,
+    iconBg: "#383E56",
+    date: "Jan 2022 - Jan 2023",
+    description: [
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged",
+    ],
+  },
 ];
 
 // const testimonials = [
@@ -176,69 +180,69 @@ const experiences = [
 // ];
 
 const projects = [
-	{
-		name: "Car Rent",
-		description:
-			"Web-based platform that allows users to search, book, and manage car rentals from various providers, providing a convenient and efficient solution for transportation needs.",
-		tags: [
-			{
-				name: "react",
-				color: "blue-text-gradient",
-			},
-			{
-				name: "mongodb",
-				color: "green-text-gradient",
-			},
-			{
-				name: "tailwind",
-				color: "pink-text-gradient",
-			},
-		],
-		image: carrent,
-		source_code_link: "https://github.com/",
-	},
-	{
-		name: "Job IT",
-		description:
-			"Web application that enables users to search for job openings, view estimated salary ranges for positions, and locate available jobs based on their current location.",
-		tags: [
-			{
-				name: "react",
-				color: "blue-text-gradient",
-			},
-			{
-				name: "restapi",
-				color: "green-text-gradient",
-			},
-			{
-				name: "scss",
-				color: "pink-text-gradient",
-			},
-		],
-		image: jobit,
-		source_code_link: "https://github.com/",
-	},
-	{
-		name: "Trip Guide",
-		description:
-			"A comprehensive travel booking platform that allows users to book flights, hotels, and rental cars, and offers curated recommendations for popular destinations.",
-		tags: [
-			{
-				name: "nextjs",
-				color: "blue-text-gradient",
-			},
-			{
-				name: "supabase",
-				color: "green-text-gradient",
-			},
-			{
-				name: "css",
-				color: "pink-text-gradient",
-			},
-		],
-		image: tripguide,
-		source_code_link: "https://github.com/",
-	},
+  {
+    name: "Car Rent",
+    description:
+      "Web-based platform that allows users to search, book, and manage car rentals from various providers, providing a convenient and efficient solution for transportation needs.",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "mongodb",
+        color: "green-text-gradient",
+      },
+      {
+        name: "tailwind",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: carrent,
+    source_code_link: "https://github.com/",
+  },
+  {
+    name: "Job IT",
+    description:
+      "Web application that enables users to search for job openings, view estimated salary ranges for positions, and locate available jobs based on their current location.",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "restapi",
+        color: "green-text-gradient",
+      },
+      {
+        name: "scss",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: jobit,
+    source_code_link: "https://github.com/",
+  },
+  {
+    name: "Trip Guide",
+    description:
+      "A comprehensive travel booking platform that allows users to book flights, hotels, and rental cars, and offers curated recommendations for popular destinations.",
+    tags: [
+      {
+        name: "nextjs",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "supabase",
+        color: "green-text-gradient",
+      },
+      {
+        name: "css",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: tripguide,
+    source_code_link: "https://github.com/",
+  },
 ];
 
 export { services, technologies, experiences, projects, aboutTech };
